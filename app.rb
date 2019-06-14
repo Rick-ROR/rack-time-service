1e817ff97f42ead804fe04dd1bf010cfbbc03c8e
Rick-ROR <Rick.ROR@ya.ru>
2019-06-14 16:09:50 +0000

    added TimeService

--- app.rb
+++ app.rb
@@ -1,20 +1,37 @@
+require_relative 'time_service'
+
 class App
 
   def call(env)
-    [status, headers, body]
+    request = Rack::Request.new(env)
+
+    if request.path_info == /time/
+      [status, headers, body]
+    else
+      request_404
+    end
   end
-end
 
-private
+  private
 
-def status
-  200
-end
+  def status
+    200
+  end
 
-def headers
-  { 'Content-Type' => 'text/plain' }
-end
+  def headers
+    { 'Content-Type' => 'text/plain' }
+  end
 
-def body
-  ["Welcome to the real World.\n"]
+  def body
+    ["Welcome to the real World.\n"]
+  end
+
+  def request_404
+    Rack::Response.new(["Page not found. Try /time?format=year\n\n"], 404, headers)
+  end
+
+  def request_time
+
+  end
+
 end
